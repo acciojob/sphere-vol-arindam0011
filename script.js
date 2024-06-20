a3be0cf7cda93f30493d6c8f966de29dd8ab564d
ARINDAM BHANDARI <136267807+arindam0011@users.noreply.github.com>
2024-06-20 21:36:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 function volume_sphere() {
     //Write your code here
-  
+	const calculate=document.getElementById("submit")
+	calculate.addEventListener("click", function(event){
+		event.preventDefault();
+		let radiusValue=document.getElementById("radius").value;
+		if(radiusValue<0){
+			return document.getElementById("volume").value="NaN"
+		}
+	const pi = Math.PI; 
+    const volume = (4 / 3) * pi * Math.pow(radiusValue, 3);
+	return document.getElementById("volume").value=volume.toFixed(4);
+	});
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
